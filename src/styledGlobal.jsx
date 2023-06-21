c59a085e5facb30c810fccb346b855e45fe9f3f5
Andressa H Cardoso <andressahenselcardoso@gmail.com>
2023-06-21 11:58:21 -0300

    Estilização body

--- src/styledGlobal.jsx
+++ src/styledGlobal.jsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import {createGlobalStyle} from "styled-components";
 
 export default createGlobalStyle`
-    
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
     hr {
         border-color: #20B2AA;
     }
